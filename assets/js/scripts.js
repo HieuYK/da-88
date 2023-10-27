46f719bcef90c382c12f777f27fe2a06d79430c2
HieuSEO <bosshieunguyens@gmail.com>
2023-10-27 14:18:37 +0700

    B61.20 - Responsive phần Navbar #1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -132,3 +132,34 @@ function handleActiveMenu() {
     dropdown.onmouseleave = () => init();
   });
 }
+
+/**
+ * JS toggle
+ *
+ * Cách dùng:
+ * Thêm class="js-toggle" toggle-target="#box" vào nơi bấm để hiện mở cái gì đó
+ * <button class="js-toggle" toggle-target="#box">Click</button>
+ * Thêm id="box" ở nơi muốn ẩn hiện
+ * <div id="box">Content show/hide</div>
+ */
+window.addEventListener("template-loaded", initJsToggle);
+
+function initJsToggle() {
+  $$(".js-toggle").forEach((button) => {
+    const target = button.getAttribute("toggle-target");
+    if (!target) {
+      document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+    }
+    button.onclick = () => {
+      if (!$(target)) {
+        return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+      }
+      const isHidden = $(target).classList.contains("hide");
+
+      requestAnimationFrame(() => {
+        $(target).classList.toggle("hide", !isHidden);
+        $(target).classList.toggle("show", isHidden);
+      });
+    };
+  });
+}
